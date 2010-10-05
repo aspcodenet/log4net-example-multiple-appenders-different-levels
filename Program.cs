efb4feea9baf2db36e92ba8b2cdc97830892b14c
Stefan Holmberg <stefan.holmberg@systementor.se>
2010-10-05 12:55:18 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net;
+
+namespace log4netexample
+{
+    class Program
+    {
+        public static ILog Log = log4net.LogManager.GetLogger("log4netexample.Program");
+        
+        static void Main(string[] args)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+
+            Log.Info("To console and file");
+            Log.Debug("Just to file");
+        }
+    }
+}
